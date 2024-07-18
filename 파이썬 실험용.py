c35c65d4391ab7ee6696b58153bbd034c5c5d399
jjungcu519 <jjungcu0519@gmail.com>
2024-07-18 11:40:53 +0900

    객체 지향 공부한 것 옮기기

--- 파이썬 실험용.py
+++ 파이썬 실험용.py
@@ -1,4 +1,5 @@
-# 클래스 정의
+
+# 클래스(붕어빵틀) 정의
 class Animal:
     def __init__(self, name, sound):
         self.name = name
@@ -7,6 +8,7 @@ class Animal:
     
     def make_sound(self):
         print(f'{self.name}은 {self.sound}하고 웁니다.')
+
 
 # 상속을 통한 클래스 확장
 class Dog(Animal):
@@ -17,7 +19,14 @@ class Dog(Animal):
     def show_info(self):
         print(f'이 개는 {self.breed}이고 이름은 {self.name}입니다.')
 
-#객체 생성 및 메서드 호출
+
+#객체(인스턴스,오브젝트) 생성
 dog1 = Dog('골댕이', '월월', '골든 리트리버')
+#속성은 name,sound,breed ......
+#cf. 메써드와 속성의 구분? : 함수는 .함수(), 속성은 .속성?
+
+
+#메서드(행위, 함수) 호출
 dog1.make_sound()
 dog1.show_info()
+
